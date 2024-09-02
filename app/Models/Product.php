d97e83bed1bc5f535d96123c1a2fdcf9c78969cc
FuSan21 <35169687+FuSan21@users.noreply.github.com>
2024-09-02 23:40:35 +0600

    fix: correct typo in Product model's primary key attribute

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,7 +9,7 @@ class Product extends Model
 {
     use HasFactory;
     protected $table = "products";
-    protected $primarykey = "bmuk_no";
+    protected $primaryKey = "bmuk_no";
     protected $fillable = ['bmuk_no', 'euro_id', 'oem_id', 'price', 'image', 'collection', 'filter_type', 'car_brand', 'model', 'specification'];
 
     public function reviews()
